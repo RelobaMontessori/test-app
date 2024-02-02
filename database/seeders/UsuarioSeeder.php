9affb39ab099759c24e9da619797efee6829459a
Guillermo Reloba <greloba@fundacionmontessori.com>
2024-02-02 18:42:52 +0100

    Empezado Seeder de usuarios

--- database/seeders/UsuarioSeeder.php
+++ database/seeders/UsuarioSeeder.php
@@ -2,7 +2,7 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Usuario;
 use Illuminate\Database\Seeder;
 
 class UsuarioSeeder extends Seeder
@@ -12,13 +12,8 @@ class UsuarioSeeder extends Seeder
      */
     public function run(): void
     {
-        DB::table('usuarios')->insert([
-            'nombre_usuario' => Str::random(15),
-            'nombre' => Str::random(15),
-            'apellidos' => Str::random(15),
-            'correo' => Str::random(10).'@mail.com',
-            'experiencia' => 'Principiante',
-            'esAdmin'=> 0
-        ]);
+        Usuario::factory()
+            ->count(50)
+            ->create();
     }
 }
